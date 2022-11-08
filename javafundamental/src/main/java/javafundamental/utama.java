2ac6d6252ead46256fb05762ad68a0739a65ad21
Kyoto <Kyoto@DESKTOP-H4K69I4>
2022-11-08 10:22:01 +0700

    class

---
+++ javafundamental/src/main/java/javafundamental/utama.java
@@ -0,0 +1,14 @@
+package javafundamental;
+
+/*
+@Author {user} a.k.a. Riky
+Junior Programmer
+Created with Eclipse version
+Created on {currentDate:date('dd-MM-yyyy HH-:mm:ss')}
+Last Modified on {currentDate:date('dd-MM-yyyy HH-:mm:ss')}
+Version 1.0
+*/
+
+public class utama {
+
+}
