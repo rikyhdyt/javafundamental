b626433567156c0c0afff863e227379af106581d
Kyoto <Kyoto@DESKTOP-H4K69I4>
2022-11-08 16:52:54 +0700

    new

---
+++ javafundamental/src/main/java/day2/coba.java
@@ -0,0 +1,18 @@
+package day2;
+
+/*
+@Author {user} a.k.a. Riky
+Junior Programmer
+Created with Eclipse version
+Created on {currentDate:date('dd-MM-yyyy HH:mm:ss')}
+Last Modified on {currentDate:date('dd-MM-yyyy HH:mm:ss')}
+Version 1.0
+*/
+
+public class coba {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
